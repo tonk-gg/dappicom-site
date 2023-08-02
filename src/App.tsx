36fd99ef8dc26ad4e228774fa798f61239d23cc4
Goblin Oats <gavin@accessorlabs.org>
2023-08-02 12:55:09 +0100

    restyle for styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import './App.css'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  width: 100vw;
+  max-height: 100vh;
+  height: 100vh;
+`
+
+const ScrollContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  overflow-y: scroll;
+  width: 100%;
+  max-width: 1920px;
+`
+
+const Segment = styled.div`
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+`
+
+const TitleSegment = styled(Segment)`
+  height: 100vh;
+`
+
+const Title = styled.h1`
+  font-family: "Alro";
+  font-size: 300px;
+`
 
 function App() {
 
   return (
     <>
-      <div className='container'>
-        <div className='scroll-container'>
-          <div className='segment'>
-            <h1 className='title full-bleed'>
-              DAPPICOM
-            </h1>
-          </div>
-          <div className='segment tv-bg'>
+    <Container>
+      <ScrollContainer>
+        <TitleSegment>
+          <Title>DAPPICOM</Title>
+        </TitleSegment>
+        <Segment>
             <img src="/images/2_PlayNESROMS.gif"/>
-          </div>
-          <div className='segment'>
-          </div>
-          <div className='segment'>
-          </div>
-        </div>
-      </div>
+        </Segment>
+      </ScrollContainer>
+    </Container>
     </>
   )
 }
